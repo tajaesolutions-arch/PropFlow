14e15a03c3009f66af85586457d671646d1677c2
tajaesolutions-arch <tajaesolutions@gmail.com>
2026-05-07 10:30:24 -0500

    Wire shared create action provider into app

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppRouter } from './routes/AppRouter.jsx';
 import { AppProvider } from './lib/AppContext.jsx';
+import { CreateActionProvider } from './components/CreateActionProvider.jsx';
 import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <AppRouter />
+      <CreateActionProvider>
+        <AppRouter />
+      </CreateActionProvider>
     </AppProvider>
   </React.StrictMode>,
 );
