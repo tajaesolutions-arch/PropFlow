d0890d0c626195ddfce86acaa70463dca76dbc11
tajaesolutions-arch <tajaesolutions@gmail.com>
2026-05-07 21:25:25 -0500

    Reorganize imports and add SmartNumberFormatting component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { AppRouter } from './routes/AppRouter.jsx';
-import { AppProvider } from './lib/AppContext.jsx';
+
 import { CreateActionProvider } from './components/CreateActionProvider.jsx';
+import { SmartNumberFormatting } from './components/SmartNumberFormatting.jsx';
+import { AppProvider } from './lib/AppContext.jsx';
+import { AppRouter } from './routes/AppRouter.jsx';
 import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
       <CreateActionProvider>
+        <SmartNumberFormatting />
         <AppRouter />
       </CreateActionProvider>
     </AppProvider>
